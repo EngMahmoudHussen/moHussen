10bcafe707085add338199e2e833c5e18aa2c187
EngMahmoudHussen <mahmoudhussen2044@gmail.com>
2024-09-18 08:31:10 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+let imgs = document.getElementById("imgs");
+let contaner = document.querySelector(".contaner");
+function phones(hoda){
+    imgs.src = hoda;
+}
+function color(hoda2){
+    contaner.style.background = hoda2;   
+}
